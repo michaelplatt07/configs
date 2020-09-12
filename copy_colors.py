99ec976210fc45569403ac60358a860ef09e2d61
michaelplatt07 <michael.platt.07@gmail.com>
2020-09-12 11:57:49 -0400

    Making directories if they don't exist.

--- copy_colors.py
+++ copy_colors.py
@@ -3,8 +3,11 @@ import os
 root_dir = '.'
 home = os.path.expanduser('~')
 
+if not os.path.exists(home + '/.config/nvim/colors'):
+    os.makedirs(home + '/.config/nvim/colors')
+
 for dir_name, sub_dir_list, file_list in os.walk(root_dir):
     for file_name in file_list:
         if '.vim' in file_name:
             copying_file_path = dir_name + '/' + file_name
-            os.system('cp ' + copying_file_path + ' ' + home + '.config/n/vim/colors/') 
+            os.system('cp ' + copying_file_path + ' ' + home + '/.config/nvim/colors/') 
