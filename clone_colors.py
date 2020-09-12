555edf9279c4813afb14a1b943ae7d42b5d2b655
michaelplatt07 <michael.platt.07@gmail.com>
2020-09-12 11:53:09 -0400

    Fixing more relative paths.

--- clone_colors.py
+++ clone_colors.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 dirname = os.path.dirname(__file__)
-with open (os.path.join(dirname, '/color_repos.csv')) as file:
+with open (os.path.join(dirname, 'color_repos.csv')) as file:
     reader = csv.reader(file, delimiter=' ', quotechar='|')
     for line in reader:
         os.system('git clone ' + line[0])
--- copy_colors.py
+++ copy_colors.py
@@ -1,9 +1,10 @@
 import os
 
 root_dir = '.'
+home = os.path.expanduser('~')
 
 for dir_name, sub_dir_list, file_list in os.walk(root_dir):
     for file_name in file_list:
         if '.vim' in file_name:
             copying_file_path = dir_name + '/' + file_name
-            os.system('cp ' + copying_file_path + ' /home/michael/.config/nvim/colors/') 
+            os.system('cp ' + copying_file_path + ' ' + home + '.config/n/vim/colors/') 
