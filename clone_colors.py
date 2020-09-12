71675e79c93832437a43c2fecc4f39425f9f4022
michaelplatt07 <michael.platt.07@gmail.com>
2020-09-12 11:49:03 -0400

    Fixing directory

--- clone_colors.py
+++ clone_colors.py
@@ -1,7 +1,8 @@
 import csv 
 import os
 
-with open ('/home/michael/Desktop/Programming/nvim_colors/color_repos.csv') as file:
+dirname = os.path.dirname(__file__)
+with open (os.path.join(dirname, '/color_repos.csv')) as file:
     reader = csv.reader(file, delimiter=' ', quotechar='|')
     for line in reader:
         os.system('git clone ' + line[0])
