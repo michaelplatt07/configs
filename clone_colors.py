ccd051198dca4ef62ab451f7e53c908f709258b8
michaelplatt07 <michael.platt.07@gmail.com>
2024-05-27 10:28:27 -0600

    Clearing out repo of old stuff

--- clone_colors.py
+++
@@ -1,8 +0,0 @@
-import csv 
-import os
-
-dirname = os.path.dirname(__file__)
-with open (os.path.join(dirname, 'color_repos.csv')) as file:
-    reader = csv.reader(file, delimiter=' ', quotechar='|')
-    for line in reader:
-        os.system('git clone ' + line[0])
--- update_colors.py
+++
@@ -1,8 +0,0 @@
-import os
-
-root_dir = '.'
-
-for dir_name, sub_dir_list, file_list in os.walk(root_dir):
-    for file_name in file_list:
-        if 'README' in file_name:
-            os.system('git -C ' + dir_name + ' pull')
