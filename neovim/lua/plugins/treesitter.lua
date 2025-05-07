973922841b641d783813ea0145d3430edd697c26
michaelplatt07 <michael@lugia.lugia>
2025-05-07 08:44:51 -0600

    Adding ensure installed to configs

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "c", "python", "html", "typescript", "javascript", "java", "vim", "vimdoc" },
 			highlight = { enable = true },
 		})
 	end,
